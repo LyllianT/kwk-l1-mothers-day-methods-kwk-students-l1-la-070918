1f020c0dacf2517ce28de7df1e27630338f6f308
LyllianT <lilyterrific2008@gmail.com>
2018-07-10 18:06:21 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,9 +1,5 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
 
-def mothers_day( hello = "Happy Mother's Day,")
-  puts "#{hello}"
-end
-
-name = mothers_day(gets.strip)
-puts name
+def mothers_day(hello = "Happy Mother's Day,")
+  
