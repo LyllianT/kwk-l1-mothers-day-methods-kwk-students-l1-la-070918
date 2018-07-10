a3ff965be0440ba76a5506d62afd50b9c7197e07
LyllianT <lilyterrific2008@gmail.com>
2018-07-10 20:03:02 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,7 +1,7 @@
 name = gets.strip
 puts "Welcome, #{name}"
 
-def holiday_greeting(on = "Fourth of July")
-  puts "Happy #{on}."
+def holiday_greeting(on = "Fourth of July, Beyonce!")
+  puts "Happy #{on}"
 end
 holiday_greeting
